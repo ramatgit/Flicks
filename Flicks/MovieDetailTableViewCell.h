ec07614fc9d475851cde99029ca5c8de34afb442
Rama Pillutla <ramat@yahoo-inc.com>
2016-09-15 00:19:53 -0700

    Detail page Image for Flicks

---
+++ Flicks/MovieDetailTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieDetailTableViewCell.h
+//  Flicks
+//
+//  Created by Rama Pillutla on 9/15/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieDetailTableViewCell : UITableViewCell
+
+@end
---
+++ Flicks/MovieDetailTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MovieDetailTableViewCell.m
+//  Flicks
+//
+//  Created by Rama Pillutla on 9/15/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import "MovieDetailTableViewCell.h"
+
+@implementation MovieDetailTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
